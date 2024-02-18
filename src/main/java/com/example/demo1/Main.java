874d5edf515d700e499d2098076f7b74fc4887eb
SimtanaIslam <simtanaislam@gmail.com>
2024-02-18 11:22:14 +0600

    RegistrationPage And Login Page Design

---
+++ src/main/java/com/example/demo1/Main.java
@@ -0,0 +1,36 @@
+package com.example.demo1;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+import static javafx.application.Application.launch;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        try {
+
+            Parent root = FXMLLoader.load(getClass().getResource("RegistrationPage.fxml"));
+            Scene scene = new Scene(root);
+            stage.setIconified(false);
+            stage.setTitle("Tour Mate");
+
+            stage.setScene(scene);
+
+            stage.show();
+
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
